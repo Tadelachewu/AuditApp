ab712b9933cee259b326f8cd2cd59a6e7103fd74
Tade Mes <tade2024bdu@gmail.com>
2025-07-04 08:14:50 +0000

    sheet.tsx:62 `DialogContent` requires a `DialogTitle` for the component

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -2,17 +2,16 @@ import 'server-only';
 import { cookies } from 'next/headers';
 import { encrypt, decrypt } from './session-crypto';
 import type { User as PrismaUser } from '@prisma/client';
-import { cache } from 'react';
 import type { User } from './definitions';
 
-export const getSession = cache(async (): Promise<User | null> => {
+export async function getSession(): Promise<User | null> {
   const sessionCookie = cookies().get('session')?.value;
   if (!sessionCookie) return null;
   
   const session = await decrypt(sessionCookie);
   
   return session;
-});
+};
 
 export async function createSession(user: PrismaUser) {
   const expires = new Date(Date.now() + 24 * 60 * 60 * 1000); // 1 day from now
